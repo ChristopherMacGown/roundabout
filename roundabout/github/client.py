b2a7968ffd8647b2968037628250f78e93319cc2
Christopher MacGown <ignoti+github@gmail.com>
2010-12-08 13:55:54 +0100

    Moved github.py over to the github package, added a config example, pylint changes

---
+++ roundabout/github/client.py
@@ -0,0 +1,40 @@
+""" A borg style github client """
+
+from __future__ import absolute_import
+from github2.client import Github
+from roundabout.config import Config
+from roundabout.github.scraper import GithubScraper, parse_pull_requests
+
+
+class Client(object):
+    """ A borg style github client """
+    __shared_state = {}
+
+    def __new__(cls, config=Config()): #pylint: disable=W0613
+        self = object.__new__(cls)
+        self.__dict__ = cls.__shared_state
+        return self
+
+    def __init__(self, config=Config()):
+        self.config = config
+        self.github = Github(username=config.github_username,
+                             api_token=config.github_api_token,
+                             requests_per_second=config.github_req_per_second)
+
+    @property
+    def issues(self):
+        """ return the list of issues from the repo """
+        return self.github.issues.list(self.config.github_repo)
+
+    @property
+    def branches(self):
+        """ Return the list of branches from the repo """
+        return self.github.repos.branches(self.config.github_repo)
+
+    @property
+    def pull_requests(self):
+        """ Use the scraper to grab any pull requests and their commit hashes. 
+        """
+        url = "https://github.com/%s/pulls" % self.config.github_repo
+        with GithubScraper(url) as response: 
+            return parse_pull_requests(response.read())

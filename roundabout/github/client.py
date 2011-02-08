2f994006a07f915964816a5e465cbac56398ebe1
Christopher MacGown <ignoti+github@gmail.com>
2011-02-08 14:43:16 +0100

    Catch NoneType exception for pull request comments without users.
    See comments attached to for an example:
      https://github.com/gem/openquake/pull/28

--- roundabout/github/client.py
+++ roundabout/github/client.py
@@ -106,10 +106,12 @@ class PullRequest(object):
         """
 
         for comment in self.discussion:
-            if comment['user']['login'] in approvers and \
-               LGTM_RE.match(comment.get('body', "")):
-                return True
-
+            try:
+                if comment['user']['login'] in approvers and \
+                   LGTM_RE.match(comment.get('body', "")):
+                    return True
+            except NoneType:
+                continue
 
     def __get_full_request(self):
         """

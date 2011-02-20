ecd14311122fba2771a3bc8c3a16f13159e34318
Christopher MacGown <ignoti+github@gmail.com>
2011-02-20 23:18:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='roundabout',
-      version='0.001',
+      version='0.10',
       description='roundabout ',
       authors=['Christopher MacGown', 'Lars Butler'],
       author_email='roundabout-dev@googlegroups.com',
       url='',
-      packages=['roundabout', 'roundabout/github', 'roundabout/git'],
-      install_requires=["coverage", "nose", "lxml", "pyyaml",
-                        "github2", "gitpython"])
+      packages=['roundabout', 'roundabout/github'],
+      install_requires=["coverage", "nose", "github2", "gitpython"])

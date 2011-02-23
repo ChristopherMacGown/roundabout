018b4234233287280d74e02a4232524c1fc52fe9
Christopher MacGown <ignoti+github@gmail.com>
2011-02-23 12:00:44 +0100

    Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='roundabout',
-      version='0.10',
+      version='0.15',
       description='roundabout ',
       authors=['Christopher MacGown', 'Lars Butler'],
       author_email='roundabout-dev@googlegroups.com',
       url='',
       packages=['roundabout', 'roundabout/github'],
       scripts = ['bin/roundabout'],
-      install_requires=["coverage", "nose", "github2", "gitpython"])
+      install_requires=["coverage", "nose", "github2", "gitpython", "pylint"])
